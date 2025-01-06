f9c395c063c9dffba7b43e0e245c87d46f60faa5
Angela Francisco <angelat.francisco@gmail.com>
2025-01-06 10:19:53 -0600

    updating text for about me, experience

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,28 +5,14 @@ const AboutMe = () => {
       <div className="container-content">
         <h2 className="section-header">about me</h2>
         <p>
-          Hello, I'm Angela! I'm a software engineer by day, cozy bookworm by
-          night. I started coding as a kid, learning html and css to customize
-          my *~ vErY cOoL ~* MySpace page.
+          Hi, I'm Angela! I'm a full-stack software engineer.
         </p>
         <p>
-          My focus is in frontend, primarily in React and TypeScript. I love
-          building interfaces at the intersection of beautiful design and
-          accessibility for all.
-        </p>
-        <p>
-          After working over a decade in the nonprofit sector, specializing in
-          database administration, I took a creative leap to pursue a career in
-          web development. I bring a passion for growing my knowledge in new
-          technologies and patience in finding the missing pieces in a puzzle
+          I strive to use my knowledge for social good and hold community at the center of my work.
         </p>
         <p>
           Offline, you'll find me reading fantasy books, exploring my
-          neighborhood by bike, or snuggling with my chiweenie pup Luya.
-        </p>
-        <p>
-          I strive to use my knowledge for social good and hold community at the
-          center of my work.
+          neighborhood by bike, or snuggling with my chiweenie pup.
         </p>
       </div>
     </div>
--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -6,6 +6,7 @@ const WorkExperience = () => {
         <h2 className="section-header">my work</h2>
         <div className="work-section-wrapper">
           <div className="work-section">
+            <p>Software Engineer Intern</p>
             <p>
               <a
                 href="https://www.useroam.io/"
@@ -13,10 +14,9 @@ const WorkExperience = () => {
                 className="link"
               >
                 Roam
-              </a>{" "}
-              | Software Engineer, Intern
+              </a>
             </p>
-            <p className="date-text">February 2024 - Present</p>
+            <p className="date-text">February 2024 - June 2024</p>
             <div className="stack-bubbles-container">
               <div className="stack-bubble">React</div>
               <div className="stack-bubble">TypeScript</div>
@@ -27,24 +27,26 @@ const WorkExperience = () => {
             </div>
             <ul className="work-section-bullet-points">
               <li>
-                Played an instrumental role in building core features at an
-                early stage startup as part of the Minimum Viable Product (MVP)
-                on a cross-functional team of engineers and UX designers.
-                Contributions include payments flow, app settings, and app
-                referrals.
+                Built core features as one of the first three engineers at
+                early-stage startup contributing to successful product launch
               </li>
               <li>
-                Contributed to the development of a component library to ensure
-                modular code based off Figma designs.
+                Developed user interfaces for four features using React,
+                TypeScript, Styled-Components, and Tailwind CSS
               </li>
               <li>
-                Wrote unit tests and integration tests for the app using Jest
-                and Playwright.
+                Collaborated closely with fellow engineers, created unit tests
+                using Jest and Playwright
+              </li>
+              <li>
+                Expanded the development of a component library to ensure
+                modular code in alignment with UX/UI designs in Figma
               </li>
             </ul>
           </div>
           <hr className="horizontal-line"></hr>
           <div className="work-section">
+            <p>Software Engineer Open-Source Contributor</p>
             <p>
               <a
                 href="https://www.mozilla.org"
@@ -52,30 +54,35 @@ const WorkExperience = () => {
                 className="link"
               >
                 Mozilla
-              </a>{" "}
-              | Software Engineer, Open-Source Contributor
+              </a>
             </p>
-            <p className="date-text">July 2022 - Present</p>
+            <p className="date-text">July 2022 - December 2022</p>
             <div className="stack-bubbles-container">
               <div className="stack-bubble">React</div>
               <div className="stack-bubble">JavaScript</div>
-              <div className="stack-bubble">CSS/SCSS</div>
+              <div className="stack-bubble">HTML/CSS</div>
+              <div className="stack-bubble">REST API</div>
+              <div className="stack-bubble">MySQL</div>
             </div>
             <ul className="work-section-bullet-points">
               <li>
-                Developed an internal dashboard for the Mozilla Privacy and
-                Security team to monitor security alerts for their library
-                automation bot using React, Protocol (Mozilla's design system),
-                REST API, and MySQL.
+                Developed an internal dashboard to monitor security alerts
+                identified by the library automation bot, increasing awareness
+                across privacy and security team
               </li>
               <li>
-                Contributed to new features, improved UI, and solved bugs for
-                Bedrock (Django app hosting Mozilla) and Protocol.
+                Built two features in adherence to the internal design system,
+                using React, REST API, and MySQL
+              </li>
+              <li>
+                Contributed to open-source pull requests and debugging for the
+                internal design system and Mozilla web application
               </li>
             </ul>
           </div>
           <hr className="horizontal-line"></hr>
           <div className="work-section">
+            <p>Database Systems Asstant Manager</p>
             <p>
               <a
                 href="https://www.oregonfoodbank.org/"
@@ -83,35 +90,76 @@ const WorkExperience = () => {
                 className="link"
               >
                 Oregon Food Bank
-              </a>{" "}
-              | Database & Systems Asst. Manager
+              </a>
             </p>
             <p className="date-text">October 2020 - September 2021</p>
             <div className="stack-bubbles-container">
+              <div className="stack-bubble">Relational SQL Databases</div>
+              <div className="stack-bubble">People Management</div>
+              <div className="stack-bubble">Data Management</div>
               <div className="stack-bubble">Data Migration</div>
-              <div className="stack-bubble">Relational Databases</div>
-              <div className="stack-bubble">SQL</div>
-              <div className="stack-bubble">Quality Assurance</div>
+              <div className="stack-bubble">Technical Training</div>
             </div>
             <ul className="work-section-bullet-points">
               <li>
-                Led a team of two database coordinators in managing daily
-                operations of the relational database and donor management
-                software.
+                Provided guidance and oversight to data entry operations of over
+                $16M yearly revenue from individuals and corporations
               </li>
               <li>
-                Co-led the data migration from a local to cloud-hosted
-                relational database in collaboration with the Development
-                Operations Manager.
+                Managed sensitive information and maintained relational SQL
+                database of more than 500K active records
               </li>
               <li>
-                Supervised quality control for the data entry team and standard
-                operating procedure documentation.
+                Spearheaded data migration project of 80TB data from local to
+                cloud-hosted database, in collaboration with team leadership and
+                IT department
               </li>
               <li>
-                Acted as the designated Point of Contact across departments and
+                Created technical training documentation and implemented
+                accessible training sessions to 10 staff and volunteers
+              </li>
+              <li>
+                Supported two direct-reports through weekly face-to-face
+                meetings, technical training, ensuring collaborative team
+                environment
+              </li>
+            </ul>
+          </div>
+          <hr className="horizontal-line"></hr>
+          <div className="work-section">
+            <p>Database Coordinator</p>
+            <p>
+              <a
+                href="https://www.oregonfoodbank.org/"
+                target="_blank"
+                className="link"
+              >
+                Oregon Food Bank
+              </a>
+            </p>
+            <p className="date-text">October 2016 - October 2020</p>
+            <div className="stack-bubbles-container">
+              <div className="stack-bubble">SQL Relational Databases</div>
+              <div className="stack-bubble">CRM Software</div>
+              <div className="stack-bubble">Microsoft Excel</div>
+              <div className="stack-bubble">Tableau</div>
+            </div>
+            <ul className="work-section-bullet-points">
+              <li>
+                Something here
+              </li>
+              <li>
+                Revised over 50 standard operating procedures to ensure accurate
+                and efficient data management, reducing inconsistencies in data
+                entry by 20%
+              </li>
+              <li>
+                Something here
+              </li>
+              <li>
+                Designated point of contact across departments and
                 teams for database queries, reporting, troubleshooting and
-                quality assurance.
+                quality assurance
               </li>
             </ul>
           </div>
