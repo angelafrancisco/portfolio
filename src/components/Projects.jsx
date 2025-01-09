3d82d0db13159556c328dba3685ab75a8b95fc81
Angela Francisco <angelat.francisco@gmail.com>
2025-01-09 12:14:43 -0600

    deleted work page, added project page

---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+const Projects = () => {
+  return (
+    <div className="section-container">
+      <div className="section-container-bar"></div>
+      <div className="container-content">
+        <h2 className="section-header">projects</h2>
+        <div className="work-section-wrapper">
+          <div className="work-section">
+            <h3>Roam: </h3>
+            <p>
+              <a
+                href="https://www.useroam.io/"
+                target="_blank"
+                className="link"
+              >
+                useRoam.io
+              </a>
+            </p>
+            <div className="stack-bubbles-container">
+              <div className="stack-bubble">React</div>
+              <div className="stack-bubble">TypeScript</div>
+              <div className="stack-bubble">Styled-Components</div>
+              <div className="stack-bubble">Tailwind CSS</div>
+              <div className="stack-bubble">Jest</div>
+              <div className="stack-bubble">Playwright</div>
+            </div>
+          </div>
+        </div>
+        <hr className="horizontal-line"></hr>
+        <div className="work-section-wrapper">
+          <div className="work-section">
+            <h3>Mozilla: </h3>
+            <p>
+              <a
+                href="https://www.mozilla.org"
+                target="_blank"
+                className="link"
+              >
+                mozilla.org
+              </a>
+            </p>
+            <div className="stack-bubbles-container">
+              <div className="stack-bubble">React</div>
+              <div className="stack-bubble">JavaScript</div>
+              <div className="stack-bubble">HTML/CSS</div>
+              <div className="stack-bubble">REST API</div>
+              <div className="stack-bubble">MySQL</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
